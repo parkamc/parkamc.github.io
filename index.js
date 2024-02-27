0e4eac6d01954d8449ff350f28889e622c267966
parkamc <161243108+parkamc@users.noreply.github.com>
2024-02-27 10:56:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ function display_image(imageUrl) {
     const imageElement = document.getElementById("image");
     imageElement.src = imageUrl;
 }
+
+function nano() {
+const apiUrl = 'https://api.meteomatics.com/2024-02-27T10:50:00.000-05:00--2024-07-31T10:50:00.000-04:00:PT5M/t_min_-300cm_1h:F/28.5656649,-81.5856742/html?model=mix';
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
